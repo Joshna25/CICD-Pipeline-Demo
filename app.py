bb1fa9a095c118f3a74611a8cf73c758e7ee9a4b
Joshna25 <130381978+Joshna25@users.noreply.github.com>
2025-04-18 11:58:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import requests
+import time
 test = "COde Smell Testing"
 x = 150
 
 def githubStatus():
     url = "https://github.com/"
     response = requests.get(url)
+    time.sleep(120)
     if response.status_code == 200:
         print("Git Hub is reachable")
     else:
